5d08ddab42c3c38e6a570c966a8c19a73bbe013b
Jacob Burroughs <maths22@gmail.com>
2014-10-10 19:12:20 -0500

    Implement enable/disable, reset, and i2c communication.

--- FCSDisplay.ino
+++ FCSDisplay.ino
@@ -12,6 +12,7 @@ const String PING = String('p'); //implemented
 const String RSET = String('r');
 const String INIT = String('i'); //implemented
 const String DSBL = String('d'); //implemented
+const String ENBL = String('e'); //implemented
 const String BRT = String('b'); //implemented
 const String WRTE_SEQ = String("ws"); //implemented
 const String WRTE_ALL = String("wa"); //implemented
@@ -20,28 +21,44 @@ const String COLN_SEQ = String("cs"); //implemented
 const String COLN_ALL = String("ca"); //implemented
 const String COLN_ONE = String("co"); //implemented
 
-const String DBG = String("e"); //implemnted
+const String DBG = String("t"); //implemnted
 
-const String ACK = String("r");
+const String ACK = String("r;");
 
 String disp[4];
 
-boolean colon[4] = {false, false, false, false};
+boolean colon[4] = {
+  false, false, false, false};
 
-int brightness;
+int brightness = 0;
+
+boolean disabled = false;
 
 String currCmd = String("");
+
+int led = 13;
 
 void setup() {
   // initialize serial:
   Serial.begin(9600);
-  
-  
+
+
   disp1.begin(0x70);
   disp2.begin(0x71);
   disp3.begin(0x72);
   disp4.begin(0x73);
 
+  disp1.setBrightness(brightness);
+  disp2.setBrightness(brightness);
+  disp3.setBrightness(brightness);
+  disp4.setBrightness(brightness);
+
+  pinMode(led, OUTPUT);
+
+  disp[0] = " d1 ";
+  disp[1] = " d2 ";
+  disp[2] = " d3 ";
+  disp[3] = " d4 ";
 }
 
 void loop() {
@@ -54,11 +71,15 @@ void loop() {
       currCmd += cmd;
       currCmd.trim();
       if(currCmd.startsWith(PING)){
-        Serial.println("ardFCSv1");
+        Serial.println("ardFCSv1;");
       }
       else if(currCmd.startsWith(BRT)){
         brightness = currCmd.substring(BRT.length()).toInt();
         Serial.println(ACK);
+        disp1.setBrightness(brightness);
+        disp2.setBrightness(brightness);
+        disp3.setBrightness(brightness);
+        disp4.setBrightness(brightness);
       }
       else if(currCmd.startsWith(COLN_ALL)){
         colon[0] = currCmd.substring(COLN_ALL.length()).toInt();
@@ -78,10 +99,10 @@ void loop() {
         for(int i = 0; i < 4; i++){
           colon[i] = currCmd.substring(pos1,pos2).toInt(); 
           pos1 = pos2+1;
-            pos2 = currCmd.substring(pos1).indexOf(',')+pos1;
-            if(pos2<pos1){
-              pos2 = currCmd.lastIndexOf(';');
-            }
+          pos2 = currCmd.substring(pos1).indexOf(',')+pos1;
+          if(pos2<pos1){
+            pos2 = currCmd.lastIndexOf(';');
+          }
         }
         Serial.println(ACK);
       }
@@ -103,18 +124,19 @@ void loop() {
         for(int i = 0; i < 4; i++){
           disp[i] = currCmd.substring(pos1,pos2); 
           pos1 = pos2+1;
-            pos2 = currCmd.substring(pos1).indexOf(',')+pos1;
-            if(pos2<pos1){
-              pos2 = currCmd.lastIndexOf(';');
-            }
+          pos2 = currCmd.substring(pos1).indexOf(',')+pos1;
+          if(pos2<pos1){
+            pos2 = currCmd.lastIndexOf(';');
+          }
         }
         Serial.println(ACK);
       }
       else if(currCmd.startsWith(DSBL)){
-        disp[0] = "    ";
-        disp[1] = "    ";
-        disp[2] = "    ";
-        disp[3] = "    ";
+        disabled = true;
+        Serial.println(ACK);
+      }
+      else if(currCmd.startsWith(ENBL)){
+        disabled = false;
         Serial.println(ACK);
       }
       else if(currCmd.startsWith(INIT)){
@@ -123,6 +145,9 @@ void loop() {
         disp[2] = " d3 ";
         disp[3] = " d4 ";
         Serial.println(ACK);
+      }
+      else if(currCmd.startsWith(RSET)){
+        asm volatile ("  jmp 0");
       }
       else if(currCmd.startsWith(DBG)){
         Serial.print(disp[0]);
@@ -144,10 +169,77 @@ void loop() {
       currCmd = String();
     } 
     else {
-
       currCmd += cmd;
     }
+  }
+  
 
+    if(colon[0]&&!disabled){
+      digitalWrite(led, HIGH);
+    } 
+    else {
+      digitalWrite(led, LOW); 
+    }
 
+    //updateBrightness
+    disp1.setBrightness(brightness);
+    disp2.setBrightness(brightness);
+    disp3.setBrightness(brightness);
+    disp4.setBrightness(brightness);
+
+    //updateColon
+    disp1.drawColon(colon[0]);
+    disp2.drawColon(colon[0]);
+    disp3.drawColon(colon[0]);
+    disp4.drawColon(colon[0]);
+
+    //updateDisp1
+    updateDigit(disp1,0,disp[0].charAt(0));
+    updateDigit(disp1,1,disp[0].charAt(1));
+    updateDigit(disp1,3,disp[0].charAt(2));
+    updateDigit(disp1,4,disp[0].charAt(3));
+
+    //updateDisp2
+    updateDigit(disp2,0,disp[1].charAt(0));
+    updateDigit(disp2,1,disp[1].charAt(1));
+    updateDigit(disp2,3,disp[1].charAt(2));
+    updateDigit(disp2,4,disp[1].charAt(3));
+
+    //updateDisp3
+    updateDigit(disp3,0,disp[2].charAt(0));
+    updateDigit(disp3,1,disp[2].charAt(1));
+    updateDigit(disp3,3,disp[2].charAt(2));
+    updateDigit(disp3,4,disp[2].charAt(3));
+
+    //updateDisp4
+    updateDigit(disp4,0,disp[3].charAt(0));
+    updateDigit(disp4,1,disp[3].charAt(1));
+    updateDigit(disp4,3,disp[3].charAt(2));
+    updateDigit(disp4,4,disp[3].charAt(3));
+
+    //writeDisplay
+    disp1.writeDisplay();
+    disp2.writeDisplay();
+    disp3.writeDisplay();
+    disp4.writeDisplay();
+}
+
+void updateDigit(Adafruit_7segment& out, int id, char c) {
+  if ((c >= '0') && (c <= '9')) {
+    out.writeDigitNum(id, c-'0');
+  } 
+  else if (c == 'b') {
+    out.writeDigitRaw(id, 0x7C);
+  } 
+  else if (c == 'r') {
+    out.writeDigitRaw(id, 0x50);
+  } 
+  else if (c == 'd') {
+    out.writeDigitRaw(id, 0x5E);
+  }
+  else {
+    Serial.println(c);
+    out.writeDigitRaw(id, 0x00);
   }
 }
+
